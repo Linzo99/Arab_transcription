3185d018cb4d843ea46a8595f3a12e0a949fb1d3
Linzo <mac@LINZO-2.local>
2023-01-22 14:59:22 +0000

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='arab_transliterator',
+    version='0.1',
+    packages=find_packages(),
+    author='Linzo99',
+    author_email='sallalioune99@gmail.com',
+    description='python script for arab text transliteration',
+    url="https://github.com/Linzo99/Arab_transcription",
+    install_requires=[]
+)
+
