6e22ecb4705134d2ed3a34a0687dd82cdbd79e23
Alioune <sallalioune@gmail.com>
2023-05-15 17:38:17 +0000

    handle some wolofal caracter

--- mapping.py
+++ mapping.py
@@ -65,4 +65,11 @@ _mapping = {
     #  alphabet.ROUNDED_HIGH_STOP_WITH_FILLED_CENTRE: u'',
     #  alphabet.SMALL_LOW_MEEM: u''
     u" ": u" ",
+    # WOLOFAL CARACTERS
+    alphabet.PEH: u'p',
+    alphabet.CEH:  u'c',
+    alphabet.GNEH: u'ñ',
+    alphabet.GAF: u'g',
+    alphabet.EH: u'é',
+    alphabet.OH: u'o'
 }
