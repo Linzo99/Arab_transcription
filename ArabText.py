d3a576b874f7024bc8181407982044ee32285bdd
Linzo99 <sallalioune@gmail.com>
2023-07-29 18:28:27 +0000

    version 1.0.0

--- ArabText.py
+++ ArabText.py
@@ -4,124 +4,125 @@ from _types.linked_queue import LinkedQueue
 
 class ArabicText(LinkedQueue):
 
-  class ArabicTextChar:
-      def __init__(self, parent, node):
-        self._node = node
-        self._parent = parent
+    class ArabicTextChar:
+        def __init__(self, parent, node):
+            self._node = node
+            self._parent = parent
 
-      def char(self):
-          return self._node._ele
+        def char(self):
+            return self._node._ele
 
-      def __eq__(self, other):
-          return self.char() == other
+        def __eq__(self, other):
+            return self.char() == other
 
-      def __str__(self):
-          return self.char()
+        def __str__(self):
+            return self.char()
 
-      def next(self, val=None):
-          if not val:
-              return self._parent.after(self)
-          ele = self
-          while val!=0 and ele.next():
-              ele = ele.next()
-              val -= 1
-          return ele
+        def next(self, val=None):
+            if not val:
+                return self._parent.after(self)
+            ele = self
+            while val != 0 and ele.next():
+                ele = ele.next()
+                val -= 1
+            return ele
 
-      def prev(self, val=None):
-          if not val:
-              return self._parent.before(self)
-          ele = self
-          while val!=0 and ele.prev():
-              ele = ele.prev()
-              val -= 1
-          return ele
+        def prev(self, val=None):
+            if not val:
+                return self._parent.before(self)
+            ele = self
+            while val != 0 and ele.prev():
+                ele = ele.prev()
+                val -= 1
+            return ele
 
-      def is_blank(self):
-          return self == ' '
+        def is_blank(self):
+            return self == ' '
 
-      def is_start(self):
-          return self._node is self._parent._head
+        def is_start(self):
+            return self._node is self._parent._head
 
-      def is_mid(self):
-          try:
-              return (not self.is_word_start()) and (not self.next().is_blank())
-          except:
-              return False
+        def is_mid(self):
+            try:
+                return (not self.is_word_start()) and (not self.next().is_blank())
+            except Exception:
+                return False
 
-      def is_end(self):
-          return self._node is self._parent._tail
+        def is_end(self):
+            return self._node is self._parent._tail
 
-      def is_word_start(self):
-          return self.prev() == u" " or (not self.prev())
+        def is_word_start(self):
+            return self.prev() == u" " or (not self.prev())
 
-      def preceeded(self, n):
-          out = u""
-          ele = self
-          while n!=0 and ele.prev():
-              out += ele.prev().char()
-              ele = ele.prev()
-              n -= 1
-          return out
+        def preceeded(self, n):
+            out = u""
+            ele = self
+            while n != 0 and ele.prev():
+                out += ele.prev().char()
+                ele = ele.prev()
+                n -= 1
+            return out
 
-      def succeeded(self, n):
-          out = u""
-          ele = self
-          while n!=0 and ele.next():
-              out += ele.next().char()
-              ele = ele.next()
-              n -= 1
-          return out
+        def succeeded(self, n):
+            out = u""
+            ele = self
+            while n != 0 and ele.next():
+                out += ele.next().char()
+                ele = ele.next()
+                n -= 1
+            return out
 
-      def is_sun(self):
-          c = self.next()
-          if not c: return False
-          if c.is_followed_by_shadda():
-              return self.next().char()
-          return False
+        def is_sun(self):
+            c = self.next()
+            if not c:
+                return False
+            if c.is_followed_by_shadda():
+                return self.next().char()
+            return False
 
-      def is_followed_by_sun(self):
-          lam_node = self.next()
-          if lam_node.next().is_followed_by_shadda():
-              return lam_node.next().char()
-          return False
+        def is_followed_by_sun(self):
+            lam_node = self.next()
+            if lam_node.next().is_followed_by_shadda():
+                return lam_node.next().char()
+            return False
 
-      def is_followed_by_shadda(self):
-          return self.next() == alphabet.SHADDA
+        def is_followed_by_shadda(self):
+            return self.next() == alphabet.SHADDA
 
-      def is_fatha_followed_by_alif(self):
-          try:
-              return self == alphabet.FATHA and self.next().is_alif()
-          except:
-              return False
+        def is_fatha_followed_by_alif(self):
+            try:
+                return self == alphabet.FATHA and self.next().is_alif()
+            except Exception:
+                return False
 
-      def is_kasra_followed_by_ya(self):
-          return self == alphabet.KASRA and self.next() == alphabet.YA
+        def is_kasra_followed_by_ya(self):
+            return self == alphabet.KASRA and self.next() == alphabet.YA
 
-      def is_damma_followed_by_waw(self):
-          return self == alphabet.DAMMA and self.next() == alphabet.WAW
+        def is_damma_followed_by_waw(self):
+            return self == alphabet.DAMMA and self.next() == alphabet.WAW
 
-  def __init__(self, text):
-      super().__init__()
-      for c in text:
-        self.enqueue(c)
+    def __init__(self, text):
+        super().__init__()
+        for c in text:
+            self.enqueue(c)
 
-  def _make_position(self, node):
-      if node is None:
+    def _make_position(self, node):
+        if node is None:
             return None
-      return self.ArabicTextChar(self, node)
+        return self.ArabicTextChar(self, node)
 
-  def first(self):
-      return self._make_position(self._head)
+    def first(self):
+        return self._make_position(self._head)
 
-  def after(self, p):
-      return self._make_position(p._node._next)
+    def after(self, p):
+        return self._make_position(p._node._next)
 
-  def before(self, p):
-      return self._make_position(p._node._prev)
+    def before(self, p):
+        return self._make_position(p._node._prev)
 
-  def __iter__(self):
-      cursor = self.first()
-      while cursor is not None:
-          self.cursor = cursor
-          yield cursor
-          cursor = self.after(cursor)
+    def __iter__(self):
+        cursor = self.first()
+        while cursor is not None:
+            self.cursor = cursor
+            yield cursor
+            cursor = self.after(cursor)
--- transliterator.py
+++ transliterator.py
@@ -2,13 +2,14 @@ import alphabet
 from mapping import _mapping
 from ArabText import ArabicText
 
+
 def normalize(text):
     # some text may have shadda coming after a vowel whic in this case
     # will fail the script, so were swapping
     text = list(text)
     for i, c in enumerate(text):
         if c == alphabet.SHADDA and (text[i-1] in alphabet.VOWELS):
-            text[i],text[i-1] = text[i-1],text[i]
+            text[i], text[i-1] = text[i-1], text[i]
     return text
 
 
@@ -28,7 +29,7 @@ class ArabTransliterator:
         for caracter in arabic_text:
 
             # handle hamza
-            if caracter in (alphabet.HAMZA, alphabet.ALIF_WITH_HAMZA_ABOVE, alphabet.ALIF_WITH_HAMZA_BELOW):
+            if caracter in alphabet.HAMZAS:
                 if caracter.is_mid():
                     out.append(u"'")
                 continue
@@ -37,12 +38,12 @@ class ArabTransliterator:
             elif caracter == alphabet.LAM:
                 sun = caracter.is_sun()
                 # handle alif lam
-                if (p:=caracter.prev()) == alphabet.ALIF:
+                if (p := caracter.prev()) == alphabet.ALIF:
                     sep = '-' if p.is_word_start() else ''
                     out.append("" if sun else "l"+sep)
 
                 # handle alif with hamzat wasl
-                elif (p:=caracter.prev()) == alphabet.ALIF_WITH_HAMZAT_WASL:
+                elif (p := caracter.prev()) == alphabet.ALIF_WITH_HAMZAT_WASL:
                     out[-1] = u"a" if p.is_start() else "l-"
 
                 else:
@@ -53,7 +54,6 @@ class ArabTransliterator:
                     out.append(sep.join([self.get(sun)]*2))
                     next(arabic_text)
                     next(arabic_text)
-                    
 
                 continue
 
@@ -97,10 +97,9 @@ class ArabTransliterator:
                     out.append(u'u')
                 continue
 
-
             # handle SHADDA
             elif caracter == alphabet.SHADDA:
-                vow = caracter.prev(2) 
+                vow = caracter.prev(2)
                 # if preceded by YA
                 if caracter.prev() == alphabet.YA:
                     if vow == alphabet.KASRA and caracter.is_mid():
@@ -118,7 +117,7 @@ class ArabTransliterator:
                         out.append(u"w")
 
                 elif caracter.prev().is_mid():
-                    if len(out)>2 and not out[-2]=="l-": 
+                    if len(out) > 2 and (not out[-2] == "l-"):
                         out.append(self.get(str(caracter.prev())))
 
             # handle the rest
@@ -132,7 +131,6 @@ class ArabTransliterator:
 
 
 if __name__ == "__main__":
-    import sys
     from pathlib import Path
     import argparse
 
